d84202333aa0c03b1ecd0db7b5dbefcf6df49d7f
Awais Ahmed <ahmed@cefriel.it>
2023-10-18 17:40:42 +0200

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Flexxible Next App",
+  description: "Discover projects ",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main className={inter.className}>
+          {children} this childeren represent page.tsx
+        </main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
